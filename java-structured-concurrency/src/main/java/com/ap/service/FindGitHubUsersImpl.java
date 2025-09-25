f424e087502865ba06651783aa327750ba205ad3
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-09-25 19:06:20 -0400

    adding more logging

--- java-structured-concurrency/src/main/java/com/ap/service/FindGitHubUsersImpl.java
+++ java-structured-concurrency/src/main/java/com/ap/service/FindGitHubUsersImpl.java
@@ -1,14 +1,13 @@
 package com.ap.service;
 
-
 import com.ap.dtos.GitHubUser;
 import com.ap.dtos.UserId;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
-
+import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.StructuredTaskScope.Joiner;
 
 public class FindGitHubUsersImpl implements FindGitHubUsersUseCase {
 
@@ -20,9 +19,14 @@ public class FindGitHubUsersImpl implements FindGitHubUsersUseCase {
         this.findGitHubUserUseCase = findGitHubUserUseCase;
     }
 
-
     @Override
     public List<GitHubUser> findGitHubUsers(List<UserId> userIds) {
-        return List.of();
+        try (var scope = StructuredTaskScope.open(Joiner.<GitHubUser>allSuccessfulOrThrow())) {
+            userIds.forEach(userId -> scope.fork(() -> findGitHubUserUseCase.findGitHubUser(userId)));
+            var result = scope.join().map(StructuredTaskScope.Subtask::get).toList();
+            return result;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
