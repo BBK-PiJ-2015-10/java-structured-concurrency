80f03610d4858ebce10bff6f13f849763bd0a77a
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-09-27 13:26:46 -0400

    fixing unit test

--- java-structured-concurrency/src/test/java/UseCaseTest.java
+++ java-structured-concurrency/src/test/java/UseCaseTest.java
@@ -62,4 +62,36 @@ public class UseCaseTest {
 
     }
 
+    @Test
+    public void test3() throws InterruptedException {
+
+        GitHubRepoRemoteImpl gitHubRemote = new GitHubRepoRemoteImpl();
+        GitHubCachedRepo gitHubCached = new GitHubCachedRepo();
+        GitHubRepo gitHubRepo = new GitHubRepoImpl(gitHubCached, gitHubRemote);
+
+        UserRepo userRepo = new UserRepoImpl();
+        FindGitHubUserUseCase useCase = new FindGitHubUserImpl(gitHubRepo, userRepo);
+        FindGitHubUsersUseCase useCases = new FindGitHubUsersImpl(useCase);
+
+        UserId userId1 = new UserId(1L);
+        UserId userId2 = new UserId(2L);
+
+        var result = useCases.findGitHubUsers(List.of(userId1, userId2));
+
+        var repositories = List.of(
+                new Repository(
+                        "raise4s", Visibility.PUBLIC, URI.create("https://github.com/rcardin/raise4s")),
+                new Repository(
+                        "sus4s", Visibility.PUBLIC, URI.create("https://github.com/rcardin/sus4s")));
+        var user1 = new User(new Username("alex"), userId1, new Email("alexis.gmail.com"));
+        var user2 = new User(new Username("alex"), userId2, new Email("alexis.gmail.com"));
+        var ghu1 = new GitHubUser(user1, repositories);
+        var ghu2 = new GitHubUser(user2, repositories);
+        var expectedResponse = List.of(ghu1, ghu2);
+
+        Assert.assertEquals(expectedResponse.stream().collect(Collectors.toSet()), result.stream().collect(Collectors.toSet()));
+
+    }
+
+
 }
